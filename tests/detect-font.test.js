5a376db36913089a17acb7df5f7e2259c6244059
Wildhoney <adam.timberlake@gmail.com>
2016-02-22 12:51:46 +0000

    Setup Babel

--- tests/detect-font.test.js
+++ tests/detect-font.test.js
@@ -1,8 +1,9 @@
+import 'babel-register';
 import test from 'ava';
 import detectFont from '../src/detect-font';
 
-test('Should be able to detect which font is being used;', () => {
+test('Should be able to detect which font is being used;', t => {
 
-
+    t.pass();
 
 });
